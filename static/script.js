a1ba827ef0fc6f979b135e6956100287834a3570
juliedosher <juliegdosher@lewisu.edu>
2023-03-20 21:30:27 -0500

    fixed url

--- static/script.js
+++ static/script.js
@@ -3,10 +3,9 @@
 const url = "https://just-enough-server.azurewebsites.net/";
 //const url = "http://localhost:3000";             // for testing purposes
 
-url += "api";
 
 async function ping() {
-    const response = await fetch(url + "/ping");       
+    const response = await fetch(url + "api/ping");       
     const data = await response.text();
     console.log(data);
 }
