9ce1ddcebb559049aa080f4d492fd5886cf84c07
juliedosher <juliegdosher@lewisu.edu>
2023-04-09 21:19:22 -0500

    added English definition functionality

--- static/script/dictionary.js
+++ static/script/dictionary.js
@@ -1,7 +1,8 @@
-const dictURL = "https://api.dictionaryapi.dev/api/v2/entries/en/"; // + word
+const dictURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"; // + word + dictKey
+const dictKey = "?key=e7da4733-26a2-4b45-b02b-e1ee552f6a84"; 
 
 const spanishURL = "https://dictionaryapi.com/api/v3/references/spanish/json/" // + word + spanishKey
-const spanishKey = "?key=dfcd5490-5ee5-41fb-821f-0979def860be";
+const spanishKey = "?key=23e33a70-d854-46ff-9d3f-27667aef8cd6";
 
 // TODO: add functions for future langauges as project progresses
 function fillFields() {
@@ -10,20 +11,18 @@ function fillFields() {
 }
 
 async function getDefinition() {
-    let apiURL = dictURL + word;
+    let word = document.getElementById('dictSearch').value;
+    let apiURL = dictURL + word + dictKey;
     const response = await fetch(apiURL);
     const jsonObj = await response.json();
-
     try {
-        let definition = jsonObj[0];
-
+        let definition = jsonObj[0].shortdef[0];
         document.getElementById("englishDefn").innerHTML = definition;
     } catch (e) {
         document.getElementById("englishDefn").innerHTML = 
         "ERROR: cannot find word";
     }
-
-    return jsonObj.definitions; // TODO: fix, replace with proper format
+    return
 }
 
 
@@ -37,12 +36,10 @@ async function getSpanish() {
         if (translation.includes(":")) {
             translation = translation.split(": ").pop();
         }
-
         document.getElementById("spanishTranslation").innerHTML = translation;
     } catch (e) {
         document.getElementById("spanishTranslation").innerHTML = 
         "ERROR: cannot find word";
     }
-    
     return;
 }
