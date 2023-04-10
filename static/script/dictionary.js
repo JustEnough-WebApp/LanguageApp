56b3f90c31db0dbdc2595e245a15b700c5301e9d
Julie Dosher <112606564+juliedosher@users.noreply.github.com>
2023-04-09 20:28:04 -0500

    added functionality to get Spanish translation

--- static/script/dictionary.js
+++ static/script/dictionary.js
@@ -1,15 +1,27 @@
 const dictURL = "https://api.dictionaryapi.dev/api/v2/entries/en/"; // + word
 
-const spanishURL = "https://dictionaryapi.com/api/v3/references/spanish/json/" // + word + api key
+const spanishURL = "https://dictionaryapi.com/api/v3/references/spanish/json/" // + word + spanishKey
 const spanishKey = "?key=dfcd5490-5ee5-41fb-821f-0979def860be";
 
 
-function getDefinition(word) {
-    let apiCall = dictURL + word;
+async function getDefinition(word) {
+    let apiURL = dictURL + word;
+    const response = await fetch(apiURL);
+    const rawData = await response.json();
+    const jsonObj = JSON.parse(rawData);
+
+
     return apiCall.definitions; // TODO: fix, replace with proper format
 }
 
-function getSpanish(word) {
-    let apiCall = spanishURL + word + spanishKey;
-    return apiCall; // TODO: check API documentation, return Spanish translation
+
+async function getSpanish() {
+    let word = document.getElementById('dictSearch').value;
+    let apiURL = spanishURL + word + spanishKey;
+    console.log("url: " + apiURL);
+    const response = await fetch(apiURL);
+    const jsonObj = await response.json();
+    
+    document.getElementById("spanishTranslation").value = jsonObj[0].shortdef[0];
+    return;
 }
