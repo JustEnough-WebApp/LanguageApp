7ccb545627b7d8b962c7a0f591d0266a85df7835
juliedosher <juliegdosher@lewisu.edu>
2023-04-09 21:08:54 -0500

    added reminder comment

--- static/script/dictionary.js
+++ static/script/dictionary.js
@@ -3,11 +3,25 @@ const dictURL = "https://api.dictionaryapi.dev/api/v2/entries/en/"; // + word
 const spanishURL = "https://dictionaryapi.com/api/v3/references/spanish/json/" // + word + spanishKey
 const spanishKey = "?key=dfcd5490-5ee5-41fb-821f-0979def860be";
 
+// TODO: add functions for future langauges as project progresses
+function fillFields() {
+    getDefinition();
+    getSpanish();
+}
 
-async function getDefinition(word) {
+async function getDefinition() {
     let apiURL = dictURL + word;
     const response = await fetch(apiURL);
     const jsonObj = await response.json();
+
+    try {
+        let definition = jsonObj[0];
+
+        document.getElementById("englishDefn").innerHTML = definition;
+    } catch (e) {
+        document.getElementById("englishDefn").innerHTML = 
+        "ERROR: cannot find word";
+    }
 
     return jsonObj.definitions; // TODO: fix, replace with proper format
 }
