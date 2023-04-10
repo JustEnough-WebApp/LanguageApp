faa12b856a23840d1c0856cd064ada3aaef7b35d
juliedosher <juliegdosher@lewisu.edu>
2023-04-09 20:47:00 -0500

    added style and error handling

--- static/script/dictionary.js
+++ static/script/dictionary.js
@@ -7,21 +7,23 @@ const spanishKey = "?key=dfcd5490-5ee5-41fb-821f-0979def860be";
 async function getDefinition(word) {
     let apiURL = dictURL + word;
     const response = await fetch(apiURL);
-    const rawData = await response.json();
-    const jsonObj = JSON.parse(rawData);
+    const jsonObj = await response.json();
 
-
-    return apiCall.definitions; // TODO: fix, replace with proper format
+    return jsonObj.definitions; // TODO: fix, replace with proper format
 }
 
 
 async function getSpanish() {
     let word = document.getElementById('dictSearch').value;
     let apiURL = spanishURL + word + spanishKey;
-    console.log("url: " + apiURL);
     const response = await fetch(apiURL);
     const jsonObj = await response.json();
+    try {
+        document.getElementById("spanishTranslation").innerHTML = jsonObj[0].shortdef[0];
+    } catch (e) {
+        document.getElementById("spanishTranslation").innerHTML = 
+        "ERROR: cannot find word";
+    }
     
-    document.getElementById("spanishTranslation").value = jsonObj[0].shortdef[0];
     return;
 }
