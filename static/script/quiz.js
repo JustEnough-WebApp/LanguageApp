c515685fc0176ead6f9f3054e665a0e791894bc5
juliedosher <juliegdosher@lewisu.edu>
2023-04-18 00:46:26 -0500

    rewrote how quiz helper functions are called

--- static/script/quiz.js
+++ static/script/quiz.js
@@ -1,3 +1,7 @@
+const url = "https://just-enough-server.azurewebsites.net/api/getQuestions"
+
+var quizData
+
 const quiz = document.getElementById('quiz')
 const question = document.getElementById('question')
 const answers = document.querySelectorAll('.answer')
@@ -6,8 +10,16 @@ const b_text = document.getElementById('b-text')
 const c_text = document.getElementById('c-text')
 const d_text = document.getElementById('d-text')
 
-let currQuestion = 0
-let quizScore = 0
+var currQuestion
+var quizScore 
+
+async function initiateQuiz() {
+    const response = await fetch(url)
+    quizData = await response.json()
+    currQuestion = 0
+    quizScore = 0
+    generateQuiz()
+}
 
 function resetSelection() {
     answers.forEach(answer => answer.checked = false)
@@ -27,48 +39,41 @@ function getAnswer() {
     return selectedAnswer
 }
 
-async function quizGenerator() {
-    //const url = "http://localhost:3000/api/getQuestions"   // for testing
-    const url = "https://just-enough-server.azurewebsites.net/api/getQuestions"
-    const response = await fetch(url)
-    const quizData = await response.json()
-
+function generateQuiz() {
     resetSelection()
     resetResult()
 
     const currData = quizData[currQuestion]
-
     question.innerHTML = currData.question
     a_text.innerHTML = currData.answer_a
     b_text.innerHTML = currData.answer_b
     c_text.innerHTML = currData.answer_c
     d_text.innerHTML = currData.answer_d
+    console.log("reached end of generate")
+}
 
-    submitBtn.addEventListener('click', () => {
-        const answer = getAnswer() 
+function submitAnswer() {
+    const answer = getAnswer() 
         if (answer === quizData[currQuestion].correct_answer) {
             result.innerHTML = `<h2>Correct!</h2>`
         } else {
             result.innerHTML = `<h2>Incorrect! The correct answer is ${quizData[currQuestion].correct_answer}. </h2>`
         }
-    })
-    
-    nextBtn.addEventListener('click', () => {
-        const answer = getAnswer() 
+}
+
+function getNextQuestion() {
+    const answer = getAnswer() 
         if (answer === quizData[currQuestion].correct_answer) {
             quizScore++
         }
         currQuestion++
     
         if(currQuestion < quizData.length) {
-            quizGenerator()
+            generateQuiz()
         } else {
             quiz.innerHTML = `
             <h2> You answered ${quizScore} out of ${quizData.length} questions correctly.</h2>
             <button type="button class="btn btn-primary btn-sm onclick="location.reload()">Reload</button>
             `
         }
-    })
 }
-
-document.getElementById("loadBtn").addEventListener("click", quizGenerator)
