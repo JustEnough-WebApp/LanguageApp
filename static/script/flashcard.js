770621ef5408ed1ca609c54ca49bdb3893999e98
juliedosher <juliegdosher@lewisu.edu>
2023-04-25 11:20:26 -0500

    flashcards work

--- static/script/flashcard.js
+++ static/script/flashcard.js
@@ -1,10 +1,10 @@
 const url = "https://just-enough-server.azurewebsites.net/api/getFlashcards"
-//const url = "http://localhost:3000/api/getQuestions";
+//const url = "http://localhost:3000/api/getQuestions";   // for testing
 
 var cardData
 
-const word = document.getElementById('word')
-const definition = document.getElementById('definition')
+const translatedWord = document.getElementById('translatedWord')
+const englishWord = document.getElementById('englishWord')
         
 var currCard
 
@@ -25,58 +25,19 @@ async function initiateCard() {
 }
 
 function generateCard() {
-
 	cardNum.innerHTML = `<h4>Word ${currCard + 1}</h4>`
 
     const currData = cardData[currCard]
-    word.innerHTML = currData.english
-    definition.innerHTML = currData.translation
-    console.log("reached end of generate")
+    translatedWord.innerHTML = currData.translation
+    englishWord.innerHTML = currData.english
 };
 
 function getNextCard() {
-
-        currCard++
-    
-        if (currCard < cardData.length) {
-            generateCard()
-        } else {`
-            <button type="button class="btn btn-primary btn-sm onclick="location.reload()">Reload</button>
-            `
-        }
-	}
-
-/*var words = ["Word 1", "Word 2", "Word 3"];
-var definitions = ["Definition 1", "Definition 2", "Definition 3"];
-var index = 0;
-
-
-
-function toggleDefinition() {
-	var definition = document.getElementById("definition");
-	if (definition.style.display === "none") {
-		definition.style.display = "block";
-		document.getElementById("show-def-button").innerHTML = "Hide Definition";
-	} else {
-		definition.style.display = "none";
-	    document.getElementById("show-def-button").innerHTML = "Show Definition";
-	}	
-}
-
-function nextCard() {
-	index++;
-	if (index >= words.length) {
-		index = 0;
+    currCard++
+    if (currCard < cardData.length) {
+        generateCard()
+    } else {
+        currCard = 0;
+        generateCard();
     }
-	document.getElementById("word").innerHTML = words[index];
-	document.getElementById("definition").innerHTML = definitions[index];
-	document.getElementById("definition").style.display = "none";
-	document.getElementById("show-def-button").innerHTML = "Show Definition";
 }
-
-document.getElementById("flashcard").addEventListener("click", function(event) {
-	if (event.target !== this) {
-		return;
-	}
-		toggleDefinition();
-});*/
