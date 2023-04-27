ca25f60c353e022e7e82066194f90181b42dcb62
juliedosher <juliegdosher@lewisu.edu>
2023-04-27 13:04:49 -0500

    data is read from modals

--- static/script/learn.js
+++ static/script/learn.js
@@ -1,39 +1,59 @@
 const url = "https://just-enough-server.azurewebsites.net/api/getList"
-//const url = "http://localhost:3000/api/getQuestions";   // for testing
+//const url = "http://localhost:3000/api/getList";   // for testing
 
 
-let languageCheckboxes = new Array(
+
+var languages = [];
+var types = [];
+
+function testChecks() {
+    languages = [];
+    types = [];
+
+    let languageCheckboxes = new Array(
     document.getElementById('french'), 
     document.getElementById('german'), 
     document.getElementById('norwegian'), 
     document.getElementById('spanish')
-);
+    );
 
-var language = [];
+    let typeCheckboxes = new Array(
+        document.getElementById('color'), 
+        document.getElementById('commonPhrase'), 
+        document.getElementById('day'), 
+        document.getElementById('emotion'),
+        document.getElementById('food'), 
+        document.getElementById('greeting'), 
+        document.getElementById('number'), 
+        document.getElementById('person'),
+        document.getElementById('place'), 
+        document.getElementById('thing'), 
+        document.getElementById('verb')
+    )
 
-function testChecks() {
-    console.log(languageCheckboxes.length)
     for (i = 0; i < languageCheckboxes.length; i++) {
-        console.log(languageCheckboxes[i].value)
-       // if (languageCheckboxes[i].checked) {
-       //     console.log(languageCheckboxes[i].value)
-       // }
+        if (languageCheckboxes[i].checked) {
+            languages.push(languageCheckboxes[i].value);
+        }
     } 
+
+    for (i = 0; i < typeCheckboxes.length; i++) {
+        if (typeCheckboxes[i].checked) {
+            types.push(typeCheckboxes[i].value);
+        }
+    }
 }
 
 
-async function initiateCard() {
-    
-
-
+async function initiateLearning() {
     const response  = await fetch(url, {
         method: 'POST',
         headers: {"Content-type": "application/json; charset=UTF-8"},
-        body: JSON.stringify({"language": language, "type": type})
+        body: JSON.stringify({"languages": languages, "types": types})
     }); 
 
-    cardData = await response.json()
+    learnData = await response.json()
 
     currCard = 0
-    generateCard()
+    //generateTable()
 }
