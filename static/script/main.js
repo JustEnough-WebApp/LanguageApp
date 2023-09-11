626c323295fa5c4b9c9bf4fe2d8130452fa8b8ea
juliedosher <juliegdosher@lewisu.edu>
2023-09-10 23:41:31 -0500

    moved javascript, fixed validation errors

---
+++ static/script/main.js
@@ -0,0 +1,15 @@
+function updateText() {
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var today = new Date();
+    var currentDay = today.getDay();
+    document.getElementById("dayOfWeek").innerHTML = daysOfWeek[currentDay];
+    setTimeout(updateText, getNextUpdate());
+}
+
+function getNextUpdate() {
+    var now = new Date();
+    var tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    var midnight = new Date(tomorrow.getFullYear(), tomorrow.getMonth(), tomorrow.getDate(), 0, 0, 0);
+    var timeUntilMidnight = midnight - now;
+    return timeUntilMidnight;
+}
