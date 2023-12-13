73a1e6fc29a3c03c0e0cd7193f92d850c6f84c5d
Julie Dosher <juliegdosher@lewisu.edu>
2023-12-13 02:59:24 -0600

    added draggable tiles

--- static/script/matching.js
+++ static/script/matching.js
@@ -1,17 +1,107 @@
 const url = "https://just-enough-server.azurewebsites.net/api/getMatching"
 //const url = "http://localhost:3000/api/getMatching";   // for testing
 
-var matchingData
-var language
-var type
-
 $.get("nav.html", function(data){
     $("#nav-placeholder").replaceWith(data);
 });
 
+
+var matchingData
+var language
+var type
+
+let englishTerms = []
+let foreignLanguageTerms = []
+
+const eSide = ["e0", "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9"]
+const flSide = ["fl0", "fl1", "fl2", "fl3", "fl4", "fl5", "fl6", "fl7", "fl8", "fl9"]
+
+const draggableListItems = document.querySelectorAll('draggable-list li')
+
+let selectedID;
+let dropTargetID;
+let count = 0;
+
+function addEventListeners() {
+    draggableListItems.forEach( item => {
+        item.addEventListener('dragStart', dragStart)
+        item.addEventListener('dragenter', dragEnter)
+        item.addEventListener('drop', dragDrop)
+        item.addEventListener('dragover', dragOver)
+        item.addEventListener('dragleave', dragLeave)
+    })
+}
+
+function dragStart() {
+    selectedID = this.id
+}
+
+function dragEnter() {
+    this.classList.add('over')
+}
+
+function dragLeave() {
+    this.classList.remove('over');
+}
+
+function dragOver(event) {
+    // default behavior prevents drop
+    event.preventDefault()
+}
+
+
+function dragDrop() {
+    dropTargetID = this.id
+    if (checkForMatch(selectedID, dropTargetID)) {
+        document.getElementById(selectedID).style.display = 'none'
+        document.getElementById(dropTargetID).style.display = 'none'
+        count++
+    }
+    this.class.remove('over')
+}
+
+function checkForMatch(selected, dropTarget) {
+    switch (selected) {
+        case e0:
+            return dropTarget.innerHTML === foreignLanguageTerms[0] ? true : false
+        
+        case e1:
+            return dropTarget.innerHTML === foreignLanguageTerms[1] ? true : false
+
+        case e2:
+            return dropTarget.innerHTML === foreignLanguageTerms[2] ? true : false
+
+        case e3:
+            return dropTarget.innerHTML === foreignLanguageTerms[3] ? true : false
+        
+        case e4:
+            return dropTarget.innerHTML === foreignLanguageTerms[4] ? true : false
+        
+        case e5:
+            return dropTarget.innerHTML === foreignLanguageTerms[5] ? true : false
+
+        case e6:
+            return dropTarget.innerHTML === foreignLanguageTerms[6] ? true : false
+
+        case e7:
+            return dropTarget.innerHTML === foreignLanguageTerms[7] ? true : false
+
+        case e8:
+            return dropTarget.innerHTML === foreignLanguageTerms[8] ? true : false
+        
+        case e9:
+            return dropTarget.innerHTML === foreignLanguageTerms[9] ? true : false
+        
+        default:
+            return false
+    }
+}
+
 async function initiateGame() {
-    var table = document.getElementById("matchingTable");
-    table.innerHTML = "";
+    // clear arrays from previous games
+    englishTerms = []
+    foreignLanguageTerms = []
+    matchingData = []
 
     const response  = await fetch(url, {
         method: 'POST',
@@ -19,29 +109,41 @@ async function initiateGame() {
         body: JSON.stringify({"language": language, "type": type})
     }); 
     matchingData = await response.json()  
-    
+
 
     if (matchingData.length > 0) {
-        console.log(matchingData)
-        var rowNum = 0;
-        
-        for (i = 0; i , matchingData.length; i++) {
-            var row = table.insertRow(rowNum);   
-            rowNum++;
-            
-            var cellEnglish = row.insertCell(0);
-            var cellTranslation = row.insertCell(1);
-        
-            cellEnglish.innerHTML = matchingData[i].english;
-            cellTranslation.innerHTML = matchingData[i].translation;
-        }   
-    }
+        for (let i = 0; i < matchingData.length; i++) {
+            englishTerms.push(matchingData[i].english)
+            foreignLanguageTerms.push(matchingData[i].translation)
+        }
 
-    else {
-        console.log("oops")
+        shuffle(foreignLanguageTerms)
+
+        for (let i = 0; i < matchingData.length; i++) {
+            document.getElementById(eSide[i]).innerHTML = englishTerms[i]
+            document.getElementById(flSide[i]).innerHTML = foreignLanguageTerms[i]
+        }
     }
-    
 }
+
+
+// Fisher-Yates shuffle algorithm
+function shuffle(arr) {
+    let currentIndex = arr.length,  randomIndex
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+  
+      // And swap it with the current element.
+      [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]]
+    }
+  
+    return arr;
+  }
 
 
 
